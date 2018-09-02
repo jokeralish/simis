fa088737c3101c939a112a7eca193eb6060ff74c
PRANKBOTS <33993971+Aprank@users.noreply.github.com>
2018-09-03 05:38:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,150 +1,37 @@
 <?php
 /*
 copyright @ medantechno.com
-Modified @ Farzain - zFz
+Modified by Ilyasa
 2017
-
 */
-
 require_once('./line_class.php');
-require_once('./unirest-php-master/src/Unirest.php');
 
-$$channelAccessToken = 'rGLmU7kWFFSW7+2K64LofStlXbOkKPE0ahpWAzt/qIjBpKhhgC6ViTRCNJyN2Iv8GBuBmzPncbsmrDGP7SH0SFiNE+KZ3texYrirn4/4pnIefVgNEpSJIo0oko2rqJmOg0/+8VX/AIn4ZQEhzuNNBgdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = '5f6a60f6771c29b326a998965f391580';//sesuaikan
+$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
+$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
 $userId 	= $client->parseEvents()[0]['source']['userId'];
-$groupId 	= $client->parseEvents()[0]['source']['groupId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
-$timestamp	= $client->parseEvents()[0]['timestamp'];
-$type 		= $client->parseEvents()[0]['type'];
-
 $message 	= $client->parseEvents()[0]['message'];
-$messageid 	= $client->parseEvents()[0]['message']['id'];
-
 $profil = $client->profil($userId);
-
-$pesan_datang = explode(" ", $message['text']);
-
-$command = $pesan_datang[0];
-$options = $pesan_datang[1];
-if (count($pesan_datang) > 2) {
-    for ($i = 2; $i < count($pesan_datang); $i++) {
-        $options .= '+';
-        $options .= $pesan_datang[$i];
-    }
-}
-#-------------------------[Function]-------------------------#
-function lirik($keyword) { 
-    $uri = "http://ide.fdlrcn.com/workspace/yumi-apis/joox?songname=" . $keyword . ""; 
- 
-    $response = Unirest\Request::get("$uri"); 
- 
-    $json = json_decode($response->raw_body, true); 
-    $result = "====[Lyrics]====";
-    $result .= "\nJudul : ";
-    $result .= $json['0']['0'];
-    $result .= "\nLyrics :\n";
-    $result .= $json['0']['5'];
-    $result .= "\n\nPencarian : Google";
-    $result .= "\n====[Lyrics]====";
-    return $result; 
-}
-#-------------------------[Function]-------------------------#
-function music($keyword) { 
-    $uri = "http://ide.fdlrcn.com/workspace/yumi-apis/joox?songname=" . $keyword . ""; 
- 
-    $response = Unirest\Request::get("$uri"); 
- 
-    $json = json_decode($response->raw_body, true); 
-    $result = "====[Music]====";
-    $result .= "\nJudul : ";
-    $result .= $json['0']['0'];
-    $result .= "\nDurasi : ";
-    $result .= $json['0']['1'];
-    $result .= "\nLink : ";
-    $result .= $json['0']['4'];
-    $result .= "\n\nPencarian : Google";
-    $result .= "\n====[Music]====";
-    return $result; 
-}
-#-------------------------[Function]-------------------------#
-#-------------------------[Function]-------------------------#
-
-# require_once('./src/function/search-1.php');
-# require_once('./src/function/download.php');
-# require_once('./src/function/random.php');
-# require_once('./src/function/search-2.php');
-# require_once('./src/function/hard.php');
-
-//show menu, saat join dan command /menu
-if ($type == 'join' || $command == '/salam') {
-    $text = "Assalamualaikum Kakak";
-    $balas = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-
-//pesan bergambar
-if($message['type']=='text') {
-	    if ($command == '/youtube') {
-        $keyword = '';
-        $image = 'https://img.youtube.com/vi/' . $keyword . '/2.jpg';
-        $balas = array(
-            'replyToken' => $replyToken,
-            'messages' => array(
-                array(
-                    'type' => 'image',
-                    'originalContentUrl' => $image,
-                    'previewImageUrl' => $image
-                ), array(
-                    'type' => 'video',
-                    'originalContentUrl' => vid_search($keyword),
-                    'previewImageUrl' => $image
-                )
-            )
-        );
-    }
-}
-if($message['type']=='text') {
-	    if ($command == '/lirik') {
-
-        $result = lirik($options);
-        $balas = array(
-            'replyToken' => $replyToken,
-            'messages' => array(
-                array(
-                    'type' => 'text',
-                    'text' => $result
-                )
-            )
-        );
-    }
-}
-if($message['type']=='text') {
-	    if ($command == '/music') {
-
-        $result = music($options);
-        $balas = array(
-            'replyToken' => $replyToken,
-            'messages' => array(
-                array(
-                    'type' => 'text',
-                    'text' => $result
-                )
-            )
-        );
-    }
+$pesan_datang = $message['text'];
+if($message['type']=='sticker')
+{	
+	$balas = array(
+							'UserID' => $profil->userId,	
+                                                        'replyToken' => $replyToken,							
+							'messages' => array(
+								array(
+										'type' => 'text',									
+										'text' => 'Terima Kasih Stikernya.'										
+									
+									)
+							)
+						);
+						
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'f1830f11-af68-49ef-bbc8-c4308cbf4d20'; //API SimSimi
+$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
@@ -200,4 +87,3 @@ if($url['result'] != 100)
 $result =  json_encode($balas);
 file_put_contents('./reply.json',$result);
 $client->replyMessage($balas);
-?>
