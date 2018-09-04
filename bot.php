058166ca7c6499559ab5e92f03e6639985f7650d
PRANKBOTS <33993971+Aprank@users.noreply.github.com>
2018-09-05 06:52:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,19 +1,31 @@
 <?php
 /*
 copyright @ medantechno.com
-Modified by Ilyasa
+Modified @ Farzain - zFz
 2017
 */
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 $channelAccessToken = '3K8JH/ylhY1y9qgpK32x4yy3IxGwiMGQHOkOgO47IyHVoY9wBJHxhCiFjeV1eEb6GBuBmzPncbsmrDGP7SH0SFiNE+KZ3texYrirn4/4pnKLG6lXlKzprykaqFX1fiWv1T0cwZFlQ7zrGYMLl8RcQgdB04t89/1O/w1cDnyilFU='; //sesuaikan 
 $channelSecret = '4f8a0b4ec3a38985eb83abaad2a41b09';//sesuaikan
-
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $userId 	= $client->parseEvents()[0]['source']['userId'];
+$groupId 	= $client->parseEvents()[0]['source']['groupId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$type 		= $client->parseEvents()[0]['type'];
 $message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
 $profil = $client->profil($userId);
+$pesan_datang = explode(" ", $message['text']);
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+
 $pesan_datang = $message['text'];
 $pesan=str_replace(" ", "%20", $pesan_datang);
 $key = 'f1830f11-af68-49ef-bbc8-c4308cbf4d20'; //API SimSimi
@@ -53,7 +65,10 @@ if($url['result'] != 100)
 						
 	}
 }
- 
-$result =  json_encode($balas);
-file_put_contents('./reply.json',$result);
-$client->replyMessage($balas);
+if (isset($balas)) {
+    $result = json_encode($balas);
+//$result = ob_get_clean();
+    file_put_contents('./balasan.json', $result);
+    $client->replyMessage($balas);
+}
+?>
